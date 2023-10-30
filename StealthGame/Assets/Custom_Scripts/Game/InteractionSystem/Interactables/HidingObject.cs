0bf0ba7e1f962c67c3c16981b6ba85d91990acaa
MrGreenMeme <lorenzflich@gmail.com>
2023-10-30 13:31:35 +0100

    Hiding Update now with Sound

--- StealthGame/Assets/Custom_Scripts/Game/InteractionSystem/Interactables/HidingObject.cs
+++ StealthGame/Assets/Custom_Scripts/Game/InteractionSystem/Interactables/HidingObject.cs
@@ -7,22 +7,45 @@ public class HidingObject : InteractableObject
 {
     private GameObject thiefModel;
     private bool thiefModelState;
+    private ParticleSystem ps;
+    public AudioClip appearSound;
+    public AudioClip disappearSound;
+
+    private float coolDown = 1;
+    float lastInputTimeStamp;
 
     private void Start()
     {
         thiefModel = GameObject.Find("SM_Chr_Business_Male_01");
         thiefModelState = thiefModel.activeSelf;
+        ps = GetComponent<ParticleSystem>();
     }
 
     public override void Interact()
     {
         base.Interact();
+
+        if(Time.time - lastInputTimeStamp < coolDown)
+        {
+            return;
+        }
+        lastInputTimeStamp = Time.time;
         //ToggleObjectHighlight(true);
 
         Thief.Instance.IsHidden = !Thief.Instance.IsHidden;
         Thief.Instance.CanMove = !Thief.Instance.IsHidden;
         //Thief.Instance.agent.isStopped = Thief.Instance.IsHidden;
         Thief.Instance.agent.SetDestination(Thief.Instance.transform.position);
+
+        ps.Play();
+        if (Thief.Instance.IsHidden)
+        {
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(disappearSound);
+
+        } else
+        {
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(appearSound);
+        }
         
         thiefModelState = !thiefModelState;
         thiefModel.SetActive(thiefModelState);
